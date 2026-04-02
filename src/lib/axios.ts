43ab90d330958f1c8376e531d38ba86f7694fd1b
sahbaz23 <amdsahbaz19@gmail.com>
2026-04-02 12:07:27 +0530

    env added

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const API = axios.create({
-  baseURL: "http://localhost:5000/api", //  backend URL
+  baseURL: process.env.NEXT_PUBLIC_API_URL, //  backend URL
 });
 
 // attach token automatically
